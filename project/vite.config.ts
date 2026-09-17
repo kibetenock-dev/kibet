15f0beb086522832b4d05a7e25aff0d71f02db37
ENOCK KIBET <enockkibet5@gmail.com>
2026-09-17 21:40:43 +0300

    Add README and GitHub Pages config

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/kibetenock/',
   plugins: [react()],
   resolve: {
     alias: {
